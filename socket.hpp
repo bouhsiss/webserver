00900c80fe03cf1effe944352f0dc857f7ddd75d
Ayoub ABOUNAKIF <aabounakif@leyton.com>
2023-03-06 17:14:57 +0100

    established a connection between a server and a client

--- socket.hpp
+++ socket.hpp
@@ -1,18 +1,29 @@
+#pragma once
+
+#include <string.h>  
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>   
+#include <arpa/inet.h>    
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
 #include <iostream>
 #include <string>
 
+
 #define PORT 8080 
 
-//Internet address (in_addr is more than just an integer because it might have more than s_addr depending on the OS, in some systems it has union)
-struct in_addr {
-	uint32_t s_addr; // holds t
-};
+// //Internet address (in_addr is more than just an integer because it might have more than s_addr depending on the OS, in some systems it has union)
+// struct in_addr {
+// 	uint32_t s_addr; // holds t
+// };
 
-// the address structure definition
-struct sockaddr_in {
-	short int sin_family; // address family
-	unsigned short int sin_port; // port number
-	struct in_addr sin_addr; // internet address
-	unsigned char sin_zero[8]; // this is included to pad the structure to the length of the struct sockaddr
-};
+// // the address structure definition
+// struct sockaddr_in {
+// 	short int sin_family; // address family
+// 	unsigned short int sin_port; // port number
+// 	struct in_addr sin_addr; // internet address
+// 	unsigned char sin_zero[8]; // this is included to pad the structure to the length of the struct sockaddr
+// };
