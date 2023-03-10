0ee3b07d62cda74be1485fb94860f3b9d344947c
Houda Bouhsiss <hbouhsis@e2r1p5.1337.ma>
2023-03-10 17:56:41 +0100

    more understanding of the server aspects

--- server.cpp
+++ server.cpp
@@ -1,51 +1,68 @@
-#include "socket.hpp"
+#include"socket.hpp"
 
-
-//server side
-
-int main(int ac, char ** av) {
-
-	struct sockaddr_in address; // transport address structure
-	memset(address.sin_zero, 0, sizeof(address));
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY is an IP add used when we don't want to bind a socket to any specific IP, when we don't know the IP add of our machine, we can use the special IP add INADDR_ANY
-	address.sin_port = htons(PORT);
-
-	// creatin a socket with a socket() system call
-	int serverSocket = socket(address.sin_family, SOCK_STREAM, 0);
-	if(serverSocket < 0)
-		std::cout << "cannot create server socket." << std::endl;
+void exchangeData(int socketClient) {
+	// send and receive messages
+	char buffer[1024] = {0};
+	int bytesReceived = recv(socketClient, buffer, 1024, 0);
+	write(1, buffer, bytesReceived);
+	if(bytesReceived <= 0)
+		std::cout << "No bytes to read" << std::endl;
 	
-	// naming the socket - assigning the transpot address to the socket 
-	if(bind(serverSocket, (struct sockaddr *)&address, sizeof(address)) < 0)
-		std::cout << "bind failed" << std::endl;
+	std::string hello = "hello from the server";
+	int bytesSent = send(socketClient, hello.c_str(), hello.size(), 0);
+	std::cout << "Sent " << bytesSent << "of " << hello.size() << std::endl;
+	std::cout << "<--------------Hello message sent from server--------------->" << std::endl;
+}
 
-	// prepare the original socket to only accept the connections (can't be used to exchange data)
-	if(listen(serverSocket, 4) < 0) {
-		std::cout << "listen Failed" << std::endl;
-		exit(EXIT_FAILURE);
+int main() {
+	std::cout << "configuring local address..." << std::endl;
+
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // indicates that the server can bind to all available network interfaces and the operating system will choose the appropriate network interface to use based on the incoming connection
+
+	struct addrinfo *bindAddress;
+	if(getaddrinfo(NULL, PORT, &hints, &bindAddress) < 0) // we use the getaddrinfo to fill in the addrinfo struct with the needed information.  <----- this function had many uses but for our purpose it generate an address that is suitable for bind() and to make it generate this we must pass in the first prameter as NULL and have the AI_PASSIVE flag set in the hints.ai_flags ------->
+	{
+		std::cout << "getaddrinfo failed" << std::endl;
+		return(EXIT_FAILURE);
+	}
+	std::cout << "Creating socket...." << std::endl;
+	int socketListen;
+	socketListen = socket(bindAddress->ai_family, bindAddress->ai_socktype, bindAddress->ai_protocol);
+	if(socketListen < 0) {
+		std::cout << "socket() failed" << std::endl;
+		return(EXIT_FAILURE);
 	}
 
-	// the accept system call grabs the first connection request on the queue and creates a new socket for that connection
-	int new_socket;
-	int addrelen = sizeof(address);
+	std::cout << "binding socket to local address.." << std::endl;
+	if(bind(socketListen, bindAddress->ai_addr, bindAddress->ai_addrlen)) {
+		std::cout << "bind() failed" << std::endl;
+		return(EXIT_FAILURE);
+	}
+	freeaddrinfo(bindAddress); //after we have bound to bindAddress we can release the address memory
+
+	std::cout << "Listening for connections" << std::endl;
+	if(listen(socketListen, 10) < 0 ) { // the second parameter tells listen() how many connections it's allowed to queue up - if 10 connection become queued up the the OS will reject new connections until we remove one from the existing queue 
+		std::cout << "listen failed" << std::endl;
+		return(EXIT_FAILURE);
+	}
+
+	struct sockaddr_in clientAddress; // store the address info for the connecting client
+	int socketClient;
+	int clientLen = sizeof(clientAddress );
 	while(1) {
 		std::cout << "<--------------Waiting for new connections -------------->" << std::endl;
-		if((new_socket = accept(serverSocket, (struct sockaddr*)&address, (socklen_t*)&addrelen)) < 0) {
+		/* accept had a few functions, first - it will block your program until a new connection is made. in other words, your program will sleep until a connection is made to the listening socket. when the connection is made accept() will create a new socket for it. it also fills in address info of the client connected */
+		if((socketClient = accept(socketListen, (struct sockaddr*)&clientAddress , (socklen_t*)&clientLen)) < 0) {
 			std::cout << "accept Failed" << std::endl;
 			exit(EXIT_FAILURE);
 		}
-
-		// send and receive messages
-		char buffer[1024] = {0};
-		int readEnd = read(new_socket, buffer, 1024);
-		std::cout << buffer << std::endl;
-		if(readEnd < 0)
-			std::cout << "No bytes to read" << std::endl;
-		
-		std::string hello = "hello from the server";
-		write(new_socket, hello.c_str(), hello.size());
-		std::cout << "<--------------Hello message sent from server--------------->" << std::endl;
-		close(new_socket);
+		std::cout << "<----------- Client is connected ---------->" << std::endl;
+		exchangeData(socketClient);
+		std::cout << "Closing connection.. " << std::endl; // if we don't close the connection the client will wait for more data until it times out.
+		close(socketClient);
 	}
 }
