268e257368377185f616221fa2ff3f72fab3ff8e
houda <bouhsisshouda@gmail.com>
2023-04-07 02:03:21 +0000

    late night drafts

--- Configuration.cpp
+++ Configuration.cpp
@@ -1,5 +1,16 @@
 #include "Configuration.hpp"
 
+void trimSpaces(std::string &line) {
+	int i = 0;
+	while(line[i] && std::isspace(line[i]))
+		i++;
+	line.erase(0, i);
+	i = line.size();
+	while(i > 0 && str::isspace(str[i-1]))
+		i--;
+	str.erase(i);
+}
+
 void Configuration::parser(char *configFilePath) {
 	std::string conf(configFilePath);
 	std::ifstream file(conf);
@@ -9,6 +20,17 @@ void Configuration::parser(char *configFilePath) {
 	}
 	std::string line;
 	while(std::getline(file, line)) {
+		//strip comments if found
+		int pos = line.find("#");
+		if(pos != string::npos)
+			line = line.substr(0, pos);
+
+		trimSpaces(line);
+
+		if(line.empty())
+			continue ;
 		
+		//check for opening/closing braces (counting)
+		//check for server/location blocks
 	}
 }
