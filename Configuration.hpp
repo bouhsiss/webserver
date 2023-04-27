4976ee12d5e420cd98076c2413004fe3287206c8
houda <bouhsisshouda@gmail.com>
2023-04-27 20:48:07 +0100

    added to dos

--- Configuration.hpp
+++ Configuration.hpp
@@ -13,3 +13,9 @@ class Configuration {
 		bool serverHasDups();
 };
 
+// add a function to check for duplicates when host and ports are similar
+// add to my parsing a check for validity of values 
+// check for validity of config file
+// assing default values if listen and port are missing and allowed methods
+// check if there's a duplication of directives or empty directives
+// loop over servers and for each server's host:port creat a socket and store
